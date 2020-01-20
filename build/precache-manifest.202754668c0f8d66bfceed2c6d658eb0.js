514c2a553e2c621fbc06553a12a0a4ba3bba7a67
Ebuka <peterdbrainy5@gmail.com>
2020-01-20 21:13:29 +0100

    added the build files

---
+++ build/precache-manifest.202754668c0f8d66bfceed2c6d658eb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abc12bb13e021fe4fab9e785f92b1047",
+    "url": "/index.html"
+  },
+  {
+    "revision": "730eecb2bfc722dc7e17",
+    "url": "/static/css/main.0bca16e3.chunk.css"
+  },
+  {
+    "revision": "85eb8db3dacf0b6e69f3",
+    "url": "/static/js/2.b4cc1eb9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.b4cc1eb9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "730eecb2bfc722dc7e17",
+    "url": "/static/js/main.5a14b3aa.chunk.js"
+  },
+  {
+    "revision": "b87a820e825a55a3d853",
+    "url": "/static/js/runtime-main.18753084.js"
+  }
+]);
